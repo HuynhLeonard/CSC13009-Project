c1f7d037a470b8868961ddf508ee6249faba7518
Leonard <thienhuuhuynh.dev@gmailcom>
2024-04-08 14:46:30 +0700

    Add library for search image, add structure for image display and List image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,4 +35,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // add dependency
+    implementation ( "com.github.bumptech.glide:glide:4.12.0" )
+    implementation ("me.xdrop:fuzzywuzzy:1.2.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    // mlkit
+    implementation ( "com.google.mlkit:image-labeling:17.0.7" )
+    implementation ( "com.google.mlkit:text-recognition:16.0.0" )
 }
