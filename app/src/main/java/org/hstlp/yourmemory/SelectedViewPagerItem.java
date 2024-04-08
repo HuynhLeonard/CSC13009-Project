7f4b1f7cbe0cea665d96f27763ddf5ada29fe161
Leonard <thienhuuhuynh.dev@gmailcom>
2024-04-08 22:55:48 +0700

    add selected view and selected picture, set wall paper,...

---
+++ app/src/main/java/org/hstlp/yourmemory/SelectedViewPagerItem.java
@@ -0,0 +1,26 @@
+package org.hstlp.yourmemory;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.File;
+
+public class SelectedViewPagerItem {
+    String selectedName;
+    Bitmap itemBitmap = null;
+    public SelectedViewPagerItem(String selectedName) {
+        this.selectedName = selectedName;
+
+    }
+    public String getSelectedName() {
+        return selectedName;
+    }
+    public Bitmap getItemBitmap() {
+
+        if (itemBitmap == null) {
+            File imgFile = new File(selectedName);
+            itemBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        }
+        return itemBitmap;
+    }
+}
